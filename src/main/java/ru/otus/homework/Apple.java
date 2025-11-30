656a4ba8615d9048ca0269edec47fa49615d3011
Madonna <dinara_93_mail@mail.ru>
2025-11-30 14:23:59 +0500

    27 обобщение

---
+++ src/main/java/ru/otus/homework/Apple.java
@@ -0,0 +1,8 @@
+package ru.otus.homework;
+
+public class Apple extends Fruit {
+
+    public Apple(String name, int helf) {
+        super(name, helf);
+    }
+}
---
+++ src/main/java/ru/otus/homework/Fruit.java
@@ -0,0 +1,19 @@
+package ru.otus.homework;
+
+public class Fruit {
+    private String name;
+    private int helf;
+
+    public Fruit (String name, int helf){
+        this.name = name;
+        this.helf=helf;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHelf() {
+        return helf;
+    }
+}
